c318bcb52ac6d2f6414b3253b9fbe18701078909
Giacomo Orsucci <orsucci.giacomo@gmail.com>
2024-05-18 11:18:07 +0200

    fixed some estetical aspects in footer and added "scrolling header" with javascript. It is the final version (I hope).

--- my-journal/src/js/main.js
+++ my-journal/src/js/main.js
@@ -1,37 +1,41 @@
 
 var scrollTriggerLarge = document.getElementById('header-collapse').scrollHeight;
+var scrollTriggerSmall = document.getElementById('mobile-header').scrollHeight;
 
 var dimensionTrigger = 768;
 var windowWidth = window.innerWidth;
 
-console.log("ciao");
-window.addEventListener('scroll', function(e) {
-    console.log(window.innerWidth);
+window.addEventListener('scroll', function() {
     if(windowWidth >= dimensionTrigger){
         if(document.documentElement.scrollTop > scrollTriggerLarge) {
             document.getElementById('fix-header').classList.remove('hidden');
             document.getElementById('fix-header').classList.add('show');
-            console.log('entro');
-
         }
         if(document.documentElement.scrollTop < scrollTriggerLarge) {
             document.getElementById('fix-header').classList.remove('show');
             document.getElementById('fix-header').classList.add('hidden');
-            console.log('entro2');
-
         }
     }else{
-
+        if(document.documentElement.scrollTop > scrollTriggerSmall) {
+            document.getElementById('mobile-scroll-header').classList.remove('hidden');
+            document.getElementById('mobile-scroll-header').classList.add('show');
+        }
+        if(document.documentElement.scrollTop < scrollTriggerSmall) {
+            document.getElementById('mobile-scroll-header').classList.remove('show');
+            document.getElementById('mobile-scroll-header').classList.add('hidden');
+        }
     }
-
 });
 
-window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function() {
     windowWidth = window.innerWidth;
     if(windowWidth < dimensionTrigger){
         document.getElementById('fix-header').classList.remove('show');
         document.getElementById('fix-header').classList.add('hidden');
-        console.log('entro3');
+    }
+    if(windowWidth > dimensionTrigger){
+        document.getElementById('mobile-scroll-header').classList.remove('show');
+        document.getElementById('mobile-scroll-header').classList.add('hidden');
     }
 });
 
